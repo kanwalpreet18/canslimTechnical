d8d7ee8b6192736a9c854936f423d532984705c7
kanwal <kanwalpreet18@gmail.com>
2015-12-26 18:13:05 +0530

    Added code to perform pattern recognition of cup and handle using kernel regression.

---
+++ kernelPattern.R
@@ -0,0 +1,176 @@
+rm(list = ls())
+
+library("np")
+library("quantmod")
+library("TTR")
+library("parallel")
+
+price <- getSymbols("SSWL.BO", env = NULL)
+save(price, file = "data.rda")
+
+load("data.rda")
+data <- price
+colnames(data) <- c("open", "high", "low", "close", "volume", "adjusted")
+
+data <- data["2014-10-20/2015-12-31"]
+
+data <- as.data.frame(data)
+data$date <- order(rownames(data))
+ 
+model.np <- npreg(close ~ date, regtype = "ll", bw = "cv.aic",
+                  gradients = TRUE, data = data)
+
+summary(model.np)
+npsigtest(model.np)
+
+data$fitted <- fitted(model.np)
+data$rpv <- NA
+data$rpv[-1] <- diff(fitted(model.np)) * data$volume
+
+data$ma.volume <- SMA(data$volume, 50)
+data$ma.rpv <- SMA(data$rpv, 50)
+data$sign.rpv <- ifelse(data$rpv > data$ma.rpv, 1, -1)
+
+T = 10 # this parameter is used for the rolling window
+good.stock = FALSE
+
+i <- 1
+t <- 51
+
+good.points <- list()
+
+while(t < (nrow(data)-T)){
+    cat("t = ", t, "\n")
+    dat <- data[data$date >= t, ]
+    
+    
+    d.k <- dat[dat$date == t, "date"]
+    p.k <- dat[dat$date == t, "fitted"]
+    
+    ## search for region K to A
+    k <- 30
+    while(k > 15){
+        cat("Searching SETUP with width =", k, "\n")
+        
+        datA <- dat[dat$date < d.k + k, ]
+        
+        ## first find absolute maxima point A
+        d.a <- datA[datA$fitted == max(datA$fitted), "date"]
+        p.a <- datA[datA$fitted == max(datA$fitted), "fitted"]
+        
+        uprv1 <- abs(mean(datA[datA$date %in% d.k:d.a & datA$rpv > 0,
+                               "rpv"],
+                          na.rm = TRUE))
+        dprv1 <- abs(mean(datA[datA$date %in% d.k:d.a & datA$rpv < 0,
+                               "rpv"],
+                          na.rm = TRUE))
+        
+        
+        if(is.na(dprv1) | is.na(uprv1))
+            alpha1 <- 0
+        else
+            alpha1 <- uprv1/dprv1
+        
+        delta <- p.a/p.k
+        
+        if(p.k < p.a & alpha1 > 1){
+            
+            cat("Okay good setup! Let's move on now... \n")
+            
+            ## search for region A to B
+            a <- 40
+            while(a > 20){
+                
+                cat("Let's search for LEFT SIDE CUP with width =", a, "\n")
+                
+                datB <- dat[dat$date < d.a + a & dat$date > d.a, ]                            
+                p.b <- datB[datB$fitted == min(datB$fitted), "fitted"]
+                d.b <- datB[datB$fitted == min(datB$fitted), "date"]
+                avg.vol <- mean(datB$volume, na.rm = TRUE)
+                avg.ma.rpv <- mean(datB$ma.rpv, na.rm = TRUE)
+                avg.ma.vol <- mean(datB$ma.volume, na.rm = TRUE)
+
+                if(p.b < p.a & avg.vol < avg.ma.vol){
+                    
+                    cat("Voila! You found the bottom, it's all uphill from here...\n")
+                    
+                    ## search for region B to C
+                    b <- 2*a
+                    while(b > round(a/3)){
+                        
+                        cat("Let's search for RIGHT SIDE CUP with width =", b, "\n")
+                        datC <- dat[dat$date > d.b & dat$date <= d.b + b, ]
+                        p.c <- datC[datC$fitted == max(datC$fitted), "fitted"]
+                        d.c <- datC[datC$fitted == max(datC$fitted), "date"]
+                        
+                        uprv2 <- abs(mean(datC[datC$rpv > 0, "rpv"], na.rm = TRUE))
+                        dprv2 <- abs(mean(datC[datC$rpv < 0, "rpv"], na.rm = TRUE))
+
+                        if(is.na(dprv2) | is.na(uprv2))
+                            alpha2 <- 0
+                        else
+                            alpha2 <- uprv2/dprv2
+                        
+                        if(p.c > p.b & alpha2 > 1){
+                            
+                            cat("Almost there... be patient now! :D \n")
+                            
+                            ## search for region C to D
+                            c <- b
+                            while(c > round(b/4)){
+                                cat("Let's search for the handle now with width =", c, "\n")
+                                cat(t, " ", k, " ", a, " ", b, " ", c, "\n")
+                                
+                                datC <- dat[dat$date > d.c & dat$date <= d.c + c, ]
+                                p.d <- datC[datC$fitted == min(datC$fitted), "fitted"]
+                                d.d <- datC[datC$fitted == min(datC$fitted), "date"]
+                                
+                                uprv3 <- abs(mean(datC[ , "rpv"], na.rm = TRUE))
+                                dprv3 <- abs(mean(datC[ , "rpv"], na.rm = TRUE))
+                                beta <- uprv2/dprv3
+                                
+                                if(p.d <= p.c & (p.d > 0.8*p.c + 0.2*p.b) &
+                                   beta > 1){
+                                    if(p.c <= p.a & p.d > p.b){
+                                        good.stock = TRUE
+                                        gamma <- log(alpha2) + log(beta) + delta
+
+                                        good.points[[i]] <- data.frame(d.k, d.a,
+                                                                       d.b, d.c, d.d,
+                                                                       gamma)
+                                        cat("Hurrah! Got ", i, " hits! \n")
+
+                                        k <- 15
+                                        a <- 20
+                                        b <- round(a/3)
+                                        c <- round(b/4)
+                                        i <- i + 1
+                                        t <- t + 15
+                                    }
+                                }
+                                c <- c - 1
+                            }
+                        }
+                        b <- b - 1
+                    }
+                }
+                a <- a - 1
+            }
+        }
+        k <- k - 1
+    }
+    t <- t + 1
+}
+
+good.points <- do.call(rbind, good.points)
+
+                                        # find the best points with highest gamma
+best.point <- c(good.points[which(good.points$gamma == max(good.points$gamma)), ])
+
+                                        # plot price-volume along with algorithm search points
+par(mfrow = c(2, 1))
+plot(data$date, data$close, cex = .1, xlab = "date", ylab = "price")
+lines(data$date, fitted(model.np), lty = 1, col = "blue")
+abline(v = best.point[1:5])
+barplot(data$volume, ylab = "volume", xlab = "date")
+
